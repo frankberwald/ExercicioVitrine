cbbe71fca6bbe27375f1df94ef33eb42c64a33c2
frankberwald <berwald1997@outlook.com>
2025-01-21 23:32:07 -0300

    setado

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,16 @@
 import { AppDataSource } from "./data-source"
+import express from "express"
+import cors from "cors"
 import { User } from "./entities/User"
 
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
 AppDataSource.initialize().then(async () => {
-
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
-
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
+    app.listen(3030, () => {
+        console.log("Server running on port 3030 (http://localhost:3030)")
+    })
 
 }).catch(error => console.log(error))

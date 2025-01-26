8dad725d40ece29b77d2b8f588bfef475321175f
frankberwald <berwald1997@outlook.com>
2025-01-26 13:41:44 -0300

    middleware pronta

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,14 +1,29 @@
-import { NextFunction, Request, Response } from "express"
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { AppDataSource } from "../data-source";
+import { User } from "../entities/User";
 
 
-const authMiddle = (req: Request, res: Response, next: NextFunction) => {
+const authMiddle = async (req: Request, res: Response, next: NextFunction) => {
   console.log(req.headers)
   const authHeader = req.headers.authorization
-  if (authHeader === "123") {
-    next()
-    return;
-  } else {
-    res.status(401).json({ message: "Unauthorized" })
+  if (authHeader) {
+    return res.status(401).json({message: "Token de autenticação ausente"})
+  }
+  const token = authHeader?.split("")[1];
+  if (!token) {
+    return res.status(401).json({message: "Token Inválido"})
+  } try {
+    const decoded: any = jwt.verify(token, process.env.JWT_SECRET || "naocontapraninguemrsrs")
+    const user = await AppDataSource.getRepository(User).findOne({where: {id: decoded.id}})
+    if (!user) {
+      return res.status(401).json({message: "Usuário não encontrado."});
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Erro de autenticação", error);
+    return res.status(401).json({message: "Token Inválido ou expirado"})
   }
 }
 

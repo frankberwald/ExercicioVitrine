e1f630c8d54d8cedae627b4a0802c138c90d26aa
frankberwald <berwald1997@outlook.com>
2025-01-26 22:03:29 -0300

    paginaçao pronta

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,29 +3,40 @@ import jwt from "jsonwebtoken";
 import { AppDataSource } from "../data-source";
 import { User } from "../entities/User";
 
+const authMiddle = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    console.log(req.headers);
 
-const authMiddle = async (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.headers)
-  const authHeader = req.headers.authorization
-  if (!authHeader) {
-    return res.status(401).json({message: "Token de autenticação ausente"})
-  }
-  const token = authHeader?.split(" ")[1];
-  if (!token) {
-    return res.status(401).json({message: "Token Inválido"})
-  } try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      res.status(401).json({ message: "Token de autenticação ausente" });
+      return;
+    }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      res.status(401).json({ message: "Token Inválido" });
+      return;
+    }
+
     const secret = process.env.JWT_SECRET || "default_jwt_secret";
     const decoded: any = jwt.verify(token, secret);
-    const user = await AppDataSource.getRepository(User).findOne({where: {id: decoded.id}})
+
+    const user = await AppDataSource.getRepository(User).findOne({
+      where: { id: decoded.id },
+    });
+
     if (!user) {
-      return res.status(401).json({message: "Usuário não encontrado."});
+      res.status(401).json({ message: "Usuário não encontrado." });
+      return;
     }
+
     req.user = user;
     next();
   } catch (error) {
     console.error("Erro de autenticação", error);
-    return res.status(401).json({message: "Token Inválido ou expirado"})
+    res.status(401).json({ message: "Token Inválido ou expirado" });
   }
-}
+};
 
-export default authMiddle
+export default authMiddle;
